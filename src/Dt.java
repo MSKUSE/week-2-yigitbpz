dd845e92332a4c83dee8895e74f560200cff0848
yigitbpz <yigitbpz@gmail.com>
2025-02-28 12:07:29 +0300

    week2

---
+++ src/Dt.java
@@ -0,0 +1,17 @@
+public class Dt {
+
+
+    public static void main(String[] args) {
+
+        int grade = 1;
+        double grade2 = 1.5;
+        boolean isok = true;
+        char first = 'A';
+
+        System.out.println(grade);
+        System.out.println(grade2);
+        System.out.println(isok);
+        System.out.println(first);
+
+    }
+}
